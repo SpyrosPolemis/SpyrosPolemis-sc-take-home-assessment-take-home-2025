9571872378d40b0afc3de2feb3cedc5ecff302f3
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2024-10-23 03:19:52 +1000

    Create folderData variable to use for all test cases

--- folder/move_folder_test.go
+++ folder/move_folder_test.go
@@ -12,6 +12,16 @@ func Test_folder_MoveFolder(t *testing.T) {
 
 	defaultOrgID := uuid.FromStringOrNil(folder.DefaultOrgID)
 
+	folderData := []folder.Folder{
+		{Name: "alpha", Paths: "alpha", OrgId: defaultOrgID},
+		{Name: "bravo", Paths: "alpha.bravo", OrgId: defaultOrgID},
+		{Name: "charlie", Paths: "alpha.bravo.charlie", OrgId: defaultOrgID},
+		{Name: "delta", Paths: "alpha.delta", OrgId: defaultOrgID},
+		{Name: "echo", Paths: "alpha.delta.echo", OrgId: defaultOrgID},
+		{Name: "foxtrot", Paths: "foxtrot", OrgId: uuid.Must(uuid.NewV4())}, // Different org
+		{Name: "golf", Paths: "golf", OrgId: defaultOrgID},
+	}
+
 	tests := [...]struct {
 		name         string
 		folders      []folder.Folder
@@ -22,49 +32,49 @@ func Test_folder_MoveFolder(t *testing.T) {
 	}{
 		{
 			name:    "Move folder without children",
-			folders: []folder.Folder{},
+			folders: folderData,
 			dst:     "parent",
 			wantErr: false,
 			want:    []folder.Folder{},
 		},
 		{
 			name:    "Move folder with children",
-			folders: []folder.Folder{},
+			folders: folderData,
 			dst:     "parent",
 			wantErr: false,
 			want:    []folder.Folder{},
 		},
 		{
 			name:    "Move folder into its child",
-			folders: []folder.Folder{},
+			folders: folderData,
 			dst:     "parent",
 			wantErr: true,
 			want:    []folder.Folder{},
 		},
 		{
 			name:    "Move folder into itself",
-			folders: []folder.Folder{},
+			folders: folderData,
 			dst:     "parent",
 			wantErr: true,
 			want:    []folder.Folder{},
 		},
 		{
 			name:    "Move folder into folder in different org",
-			folders: []folder.Folder{},
+			folders: folderData,
 			dst:     "parent",
 			wantErr: true,
 			want:    []folder.Folder{},
 		},
 		{
 			name:    "Move folder that does not exist",
-			folders: []folder.Folder{},
+			folders: folderData,
 			dst:     "parent",
 			wantErr: true,
 			want:    []folder.Folder{},
 		},
 		{
 			name:    "Move folder into destination that does not exist",
-			folders: []folder.Folder{},
+			folders: folderData,
 			dst:     "parent",
 			wantErr: true,
 			want:    []folder.Folder{},
