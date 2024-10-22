40a29cc2729eaa67bf7917397d19da5af2e8eab8
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2024-10-22 22:57:00 +1000

    Add error checking to handle blank name or orgID to GetAllChildFolders method

--- folder/get_folder.go
+++ folder/get_folder.go
@@ -1,6 +1,7 @@
 package folder
 
 import (
+	"errors"
 	"github.com/gofrs/uuid"
 	"strings"
 )
@@ -23,8 +24,18 @@ func (f *driver) GetFoldersByOrgID(orgID uuid.UUID) []Folder {
 
 }
 
-func (f *driver) GetAllChildFolders(orgID uuid.UUID, name string) []Folder {
+func (f *driver) GetAllChildFolders(orgID uuid.UUID, name string) ([]Folder, error) {
 	// Return slice containing all child folders of selected folder
+
+	// Error checking
+	if name == "" {
+		return []Folder{}, errors.New("name is required")
+	}
+	if orgID == uuid.Nil {
+		return []Folder{}, errors.New("orgID is required")
+	}
+
+	// Get child folders
 	folders := f.folders
 	childFolders := []Folder{}
 	for _, f := range folders {
@@ -32,5 +43,5 @@ func (f *driver) GetAllChildFolders(orgID uuid.UUID, name string) []Folder {
 			childFolders = append(childFolders, f)
 		}
 	}
-	return childFolders
+	return childFolders, nil
 }
