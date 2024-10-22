58774b8f029fed0fe601752d550e7195b8bd1c86
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2024-10-22 22:52:12 +1000

    Add method name to comments to improve clarity

--- folder/folder.go
+++ folder/folder.go
@@ -5,12 +5,12 @@ import "github.com/gofrs/uuid"
 type IDriver interface {
 	// GetFoldersByOrgID returns all folders that belong to a specific orgID.
 	GetFoldersByOrgID(orgID uuid.UUID) []Folder
-	// component 1
+	// GetAllChildFolders component 1
 	// Implement the following methods:
 	// GetAllChildFolders returns all child folders of a specific folder.
-	GetAllChildFolders(orgID uuid.UUID, name string) []Folder
+	GetAllChildFolders(orgID uuid.UUID, name string) ([]Folder, error)
 
-	// component 2
+	// MoveFolder component 2
 	// Implement the following methods:
 	// MoveFolder moves a folder to a new destination.
 	MoveFolder(name string, dst string) ([]Folder, error)
